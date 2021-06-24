39fe490bed14f6b524f1313134fb2bd931e9e02d
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-06-24 21:15:33 +0300

    Create CarBrands.java

---
+++ CarBrands.java
@@ -0,0 +1,13 @@
+package foreachmethod;
+
+import java.util.*;
+
+public class CarBrands {
+	public static void main(String[] args) {
+		String[] car_brands = {"Volvo","Mustang","Mercedes"};
+		for(String arrays_object : car_brands) {
+			System.out.println(arrays_object);
+		}
+	}
+
+}
